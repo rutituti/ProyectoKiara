ead0d71b3e48704e0e740d47681792043bb43483
RUTH S <ruthsolisvelasco@outlook.com>
2022-10-15 20:23:24 -0500

    Caso de uso consultar Cronograma en fase BETA, se despliega el cronograma segun el cliente y propiedad asociados (modificaciones en codigo, implementar UI) Ajustes realizados en UI y correciones de sidebar

--- routes/inicio.routes.js
+++ routes/inicio.routes.js
@@ -3,27 +3,19 @@ const express = require('express');
 const router = express.Router();
 
 const inicioControllers = require("../controllers/inicio.controller");
-const acontroller = require("../controllers/a_controller");
-const bcontroller = require("../controllers/b_controller");
 
 router.get('/QuienesSomos', inicioControllers.get_Infoempresa); //Muestra la informacion de la empresa
 
-router.get('/perfil', inicioControllers.getperfil); //Muestra la informacion de la empresa
+
+// /user/mispropiedades
 
 router.get('/propiedades/venta', inicioControllers.get_casas_venta); //Mostrar casas en venta
 
 router.get('/propiedades/renta', inicioControllers.get_casas_renta); //Mostrar casas en renta
 
-//router.get('/propiedades/new', inicioControllers.get_new); //Formulario registro de propiedad
-
-router.get('/propiedades/new', acontroller.getCliente); //Formulario registro de propiedad, obtner propiedades
-router.get('/propiedades/new', acontroller.post_new); //Formulario registro de propiedad
-
-router.get('/muestra/cliente/:id',bcontroller.getClientes);  // muestra cliente prototipo hay que borrarla solo comprueba el fuc BD
+router.get('/propiedades/new', inicioControllers.get_new); //Formulario registro de propiedad
 
 router.get('/propiedades', inicioControllers.get_casas); //Mostrar listado de propiedades totales
-
-router.get('/sesion', inicioControllers.get_sesion) //Mostrar Inicio de Sesion
 
 // Mandar pagina de inicio
 router.get('/', inicioControllers.get_root );
