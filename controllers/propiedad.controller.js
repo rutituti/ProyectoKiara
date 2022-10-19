73f9a5c5ade9b9e267fe2eda7fafc86cbe403533
genaro-ambia <A01420567@tec.mx>
2022-10-19 13:29:12 -0500

    añadido bodyparser para json(ajax)

--- controllers/propiedad.controller.js
+++ controllers/propiedad.controller.js
@@ -1,3 +1,14 @@
 const path = require('path');
 const Propiedad = require('../models/propiedad.model');
 
+exports.get_buscar =  (request, response, next) => {
+    
+    Propiedad.find(request.params.valor_busqueda)
+        .then( ([rows, fieldData]) => {
+            response.status(200).json(rows);
+        }).catch( (error) => {
+            console.log(error);
+        });
+
+};
+
