6c70e74f9c507faeb8c5588c9bd267ac5fdd11fd
genaro-ambia <A01420567@tec.mx>
2022-10-19 14:41:25 -0500

    comentarios al modelo propiedad

--- controllers/propiedad.controller.js
+++ controllers/propiedad.controller.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const Propiedad = require('../models/propiedad.model');
 
+
+
+//Método de búsqueda de propiedad para ser implementado con Ajax
+
 exports.get_buscar =  (request, response, next) => {
     
     Propiedad.find(request.params.valor_busqueda)
