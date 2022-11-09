4e7e8909b339446f54906c6014c71b17d47139b7
Arturo Valencia Acosta <A01274389@tec.mx>
2022-11-08 23:56:20 -0600

    Se hacen las vistas dinamicas de los documentos y se agrega el storeprocedure

--- controllers/operacion.controller.js
+++ controllers/operacion.controller.js
@@ -49,26 +49,99 @@ exports.get_seg = (request, response, next) => {
 
 
 exports.get_segexp = (request, response, next) => {
-    
-    ExpedienteRenta.fetchDocsArrendador()
-    .then(([rows, fieldData]) => {
-        console.log(rows[0]);
-      
-        response.render(path.join('..','views','op_venta','expediente.ejs'), {
-            numdocs : rows,
-            info: info,
-            isLoggedIn: request.session.isLoggedIN ? request.session.isLoggedIN : false,
-            user: request.session.user ? request.session.user : '',
-            ubicacion: request.session.ubicacion ? request.session.ubicacion : '',
-           
+    request.session.ubicacion = request.params.operacion;
+    request.session.ID_tipoExpArrendatario = 1;
+ //   console.log(request.session.ID_tipoExpArrendatario);
+//  console.log(request.session.ubicacion);
+    if (request.session.ubicacion === 'alquilar')
+    {
+            //  <h5> Expediente Arrendatario </h5>
+            ExpedienteRenta.fetchDocsVendedor(5)
+            .then(([rows, fieldData]) => {
+             console.log(rows[0]);
+            //    console.log(tipoExpCliente[0])
+                response.render(path.join('..','views','op_venta','expediente.ejs'), {
+                    numdocs : rows[0],
+                    info: info,
+                    isLoggedIn: request.session.isLoggedIN ? request.session.isLoggedIN : false,
+                    user: request.session.user ? request.session.user : '',
+                    ubicacion: request.session.ubicacion ? request.session.ubicacion : '',
+                    nombre: request.session.nombre ? request.session.nombre : '',
+                    
 
-        }); 
- 
-    })
-    .catch( error => { 
-        console.log(error)
-    });
-    
+                }); 
+        
+            })
+            .catch( error => { 
+                console.log(error)
+            });
+    }else if(request.session.ubicacion === 'renta')
+    {
+                        //  <h5> Expediente Arrendatario </h5>
+                        ExpedienteRenta.fetchDocsVendedor(7)
+                        .then(([rows, fieldData]) => {
+                            console.log(rows[0]);
+                        
+                            response.render(path.join('..','views','op_venta','expediente.ejs'), {
+                                numdocs : rows[0],
+                                info: info,
+                                isLoggedIn: request.session.isLoggedIN ? request.session.isLoggedIN : false,
+                                user: request.session.user ? request.session.user : '',
+                                ubicacion: request.session.ubicacion ? request.session.ubicacion : '',
+                                nombre: request.session.nombre ? request.session.nombre : '',
+            
+                            }); 
+                    
+                        })
+                        .catch( error => { 
+                            console.log(error)
+                        });
+    } else if(request.session.ubicacion === 'venta')
+    {
+                        //  <h5> Expediente Arrendatario </h5>
+                        ExpedienteRenta.fetchDocsVendedor(1)
+                        .then(([rows, fieldData]) => {
+                            console.log(rows[0]);
+                        
+                            response.render(path.join('..','views','op_venta','expediente.ejs'), {
+                                numdocs : rows[0],
+                                info: info,
+                                isLoggedIn: request.session.isLoggedIN ? request.session.isLoggedIN : false,
+                                user: request.session.user ? request.session.user : '',
+                                ubicacion: request.session.ubicacion ? request.session.ubicacion : '',
+                                nombre: request.session.nombre ? request.session.nombre : '',
+            
+                            }); 
+                    
+                        })
+                        .catch( error => { 
+                            console.log(error)
+                        });
+    }
+   
+    else if(request.session.ubicacion === 'compra')
+    {
+                        //  <h5> Expediente Arrendatario </h5>
+                        ExpedienteRenta.fetchDocsVendedor(3)
+                        .then(([rows, fieldData]) => {
+                            console.log(rows[0]);
+                        
+                            response.render(path.join('..','views','op_venta','expediente.ejs'), {
+                                numdocs : rows[0],
+                                info: info,
+                                isLoggedIn: request.session.isLoggedIN ? request.session.isLoggedIN : false,
+                                user: request.session.user ? request.session.user : '',
+                                ubicacion: request.session.ubicacion ? request.session.ubicacion : '',
+                                nombre: request.session.nombre ? request.session.nombre : '',
+            
+                            }); 
+                    
+                        })
+                        .catch( error => { 
+                            console.log(error)
+                        });
+    }
+
 };
 exports.get_operacion = (request, response, next) => {
     request.session.ubicacion = request.params.operacion;
--- models/expedienteRenta.js
+++ models/expedienteRenta.js
@@ -13,33 +13,10 @@ module.exports = class Expediente {
 }
 
 //Documentos necesarios por el Vendedor
-static fetchDocsVendedor() {
-    return db.execute('SELECT Te.tipoExpCliente , R.ID_tipoExpC, R.ID_tipoDocC, Td.Nombre FROM Tipo_ExpCliente Te, Restriccion_TED_cliente R, Tipo_docCliente Td  WHERE  R.ID_tipoExpC = Te.ID AND Te.ID=1  AND Td.ID = R.ID_tipoDocC');
+static fetchDocsVendedor(ID_TiEC) {
+    return db.execute('CALL get_docs(?);',[ID_TiEC]);
 }
-//Documentos necesarios por el CopropiatarioVendedor
-static fetchDocsCopVendedor() {
-    return db.execute('SELECT Te.tipoExpCliente , R.ID_tipoExpC, R.ID_tipoDocC, Td.Nombre  FROM Tipo_ExpCliente Te, Restriccion_TED_cliente R, Tipo_docCliente Td  WHERE  R.ID_tipoExpC = Te.ID AND Te.ID=2  AND Td.ID = R.ID_tipoDocC');
-}
-//Documentos necesarios por el Comprador
-static fetchDocsComprador() {
-    return db.execute('SELECT Te.tipoExpCliente , R.ID_tipoExpC, R.ID_tipoDocC, Td.NombreFROM Tipo_ExpCliente Te, Restriccion_TED_cliente R, Tipo_docCliente Td WHERE  R.ID_tipoExpC = Te.ID AND Te.ID=3  AND Td.ID = R.ID_tipoDocC');
-}
-//Documentos necesarios por el CopropietarioVendedor
-static fetchDocsCopComprador() {
-    return db.execute('SELECT Te.tipoExpCliente , R.ID_tipoExpC, R.ID_tipoDocC, Td.Nombre   FROM Tipo_ExpCliente Te, Restriccion_TED_cliente R, Tipo_docCliente Td WHERE  R.ID_tipoExpC = Te.ID AND Te.ID=4  AND Td.ID = R.ID_tipoDocC');
-}
-//Documentos necesarios por el arrendatario
-static fetchDocsArrendatario() {
-      return db.execute('SELECT Te.tipoExpCliente , R.ID_tipoExpC, R.ID_tipoDocC, Td.Nombre FROM Tipo_ExpCliente Te, Restriccion_TED_cliente R, Tipo_docCliente Td WHERE  R.ID_tipoExpC = Te.ID AND Te.ID=5  AND Td.ID = R.ID_tipoDocC');
-  }
-//Documentos necesarios por el Obligado Solidario
-static fetchDocsObligadoSolidario() {
-    return db.execute('SELECT Te.tipoExpCliente , R.ID_tipoExpC, R.ID_tipoDocC, Td.Nombre FROM Tipo_ExpCliente Te, Restriccion_TED_cliente R, Tipo_docCliente Td WHERE  R.ID_tipoExpC = Te.ID AND Te.ID=6  AND Td.ID = R.ID_tipoDocCSELECT * FROM Tipo_ExpCliente Te, Restriccion_TED_cliente R, Tipo_docCliente Td WHERE  R.ID_tipoExpC = Te.ID AND Te.ID=6  AND Td.ID = R.ID_tipoDocC');
-}
-//Documentos necesarios por el arrendador
-static fetchDocsArrendador() {
-    return db.execute('SELECT Te.tipoExpCliente , R.ID_tipoExpC, R.ID_tipoDocC, Td.Nombre FROM Tipo_ExpCliente Te, Restriccion_TED_cliente R, Tipo_docCliente Td WHERE  R.ID_tipoExpC = Te.ID AND Te.ID=7  AND Td.ID = R.ID_tipoDocC');
-}
+
   //Este método servirá para guardar de manera persitente el nuevo objeto
         //Inserta un nuevo elemento en la tabla
   /*    save(){   
