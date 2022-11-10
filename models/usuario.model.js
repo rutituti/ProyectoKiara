57c47712ae8a44d85d001ea7ff7801e74ca84f3e
genaro-ambia <A01420567@tec.mx>
2022-11-10 17:06:07 -0600

    modelo borrar usuario

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -56,6 +56,15 @@ module.exports = class Usuario {
         [username]);
   }
 
+  //Stored procedure para borrar un usuario
+
+  static deleteUser(username){
+    return db.execute(
+      'CALL delete_usuario(?)',
+      [username]
+    );
+  }
+
 }
 
   
