e06ef6fe6a71a11e2d88abf59da816dedfd994f2
Arturo Valencia Acosta <A01274389@tec.mx>
2022-11-10 23:12:08 -0600

    Se agregaran varias vistas y se imprimen los documentos

--- app.js
+++ app.js
@@ -6,14 +6,37 @@ const path = require('path');
 const cookieParser = require('cookie-parser');// uso de cookies
 const session = require('express-session'); // Trabajar con sessiones
 
+const multer = require('multer'); // archivos 
+
+var cont=0; 
+
 // haciendo uso de EJS motor de templetes
 app.set('view engine','ejs');
 app.set('views','views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+//En el registro, pasamos la constante de configuración y
+//usamos single porque es un sólo archivo el que vamos a subir, 
+//pero hay diferentes opciones si se quieren subir varios archivos. 
+//'archivo' es el nombre del input tipo file de la forma
 
+app.use(bodyParser.json());
+//fileStorage: Es nuestra constante de configuración para manejar el almacenamiento
+const fileStorage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        //'uploads': Es el directorio del servidor donde se subirán los archivos 
+        callback(null, 'public/uploads');
+    },
+    filename: (request, file, callback) => {
+        //aquí configuramos el nombre que queremos que tenga el archivo en el servidor, image.png
+        //para que no haya problema si se suben 2 archivos con el mismo nombre concatenamos el timestamp
+        callback(null,new Date().getDay() +'' + new Date().getMonth() +'' +new Date().getFullYear() +'' +new Date().getHours() +'' +new Date().getMinutes() +'' +  new Date().getSeconds() +''  + file.originalname);
+    },
+});
+
+
+app.use(multer({ storage: fileStorage }).any('doc')); 
 
 app.use(cookieParser());
 
