689c5bf5bd2806d8866161cbf12ff7a933843f72
Arturo Valencia Acosta <A01274389@tec.mx>
2022-11-15 17:38:44 -0600

    Se agregan los push de la primera consulta

--- controllers/operacion.controller.js
+++ controllers/operacion.controller.js
@@ -49,9 +49,9 @@ exports.get_seg = (request, response, next) => {
 
 exports.get_segexp = (request, response, next) => {
     request.session.ubicacion = request.params.operacion;
-    console.log(request.session.ubicacion_documento);
+   // console.log(request.session.ubicacion_documento);
     request.session.ubicacion_documento = request.params.IDtipoDocCliente;
-    console.log(request.session.ubicacion);
+ //   console.log(request.session.ubicacion);
  
 
     
@@ -61,9 +61,16 @@ exports.get_segexp = (request, response, next) => {
 
         ExpedienteRenta.fetchDocsVendedor(ExpedienteRenta.EXPEDIENTE_ARRENDATARIO)
             .then(([rows, fieldData]) => {
+                for(let r of rows[0])
+                {
                     
+                    request.session.id_tip_expC=r.IDtipoExpCliente;
+                    request.session.id_tip_DdocC=r.IDtipoDocCliente;
+                    
+                }
+                   
              //   request.session.documento = rows[0].Nombre;
-                console.log( rows[0].Nombre);
+                //console.log( rows[0].Nombre);
                // console.log(rows[0]);   
 
             
@@ -190,8 +197,8 @@ exports.post_exp = (request, response, next) => {
         console.log(request.files[cont].filename);
 
         let expediente = new ExpedienteRenta(request.session.user, r.tipoDoc, r.tipoExpCliente, 'En Revision', request.files[cont].filename);
-        //expediente.save().then().catch(err => console.log(err));   
-        cont = cont+1;
+        expediente.save().then().catch(err => console.log(err));   
+        
     }
 
     response.redirect('/inicio');
@@ -264,75 +271,14 @@ exports.get_operacion = (request, response, next) => {
 };
 
 
-exports.get_operacion = (request, response, next) => {
-    request.session.ubicacion = request.params.operacion;
-    if (request.session.ubicacion === 'renta' || request.session.ubicacion === 'venta')
-    {
-        Proceso_CV.fetch_casasVR_idC(request.session.user,request.params.operacion)
-            .then(([rows, fieldData]) => {
-                //console.log(request.session.info);
-                console.log("GET CASA EN VENTA");
-                //console.log(rows);
-                response.render(path.join('..','views','op_venta','casasV.ejs'), {
-                    casas_V: rows[0],
-                    info: request.session.info ? request.session.info : '',
-                    isLoggedIn: request.session.isLoggedIN ? request.session.isLoggedIN : false,
-                    user: request.session.user ? request.session.user : '',
-                    ubicacion: request.session.ubicacion ? request.session.ubicacion : '',
-                    nombre: request.session.nombre ? request.session.nombre : '',
-                    registro: request.session. registro ? request.session. registro : '',
 
-                }); 
-            }).catch( error => { 
-            console.log(error);
-            });
-
-    }else if (request.session.ubicacion === 'alquilar' )
-    {
-        Proceso_CV.fetch_casasA_idC(request.session.user)
-                .then(([rows, fieldData]) => {
-                    
-                    response.render(path.join('..','views','op_venta','casasV.ejs'), {
-                        casas_V: rows[0],
-                        info: request.session.info ? request.session.info : '',
-                        isLoggedIn: request.session.isLoggedIN ? request.session.isLoggedIN : false,
-                        user: request.session.user ? request.session.user : '',
-                        ubicacion: request.session.ubicacion ? request.session.ubicacion : '',
-                        nombre: request.session.nombre ? request.session.nombre : '',
-                        registro: request.session. registro ? request.session. registro : '',
-
-                    }); 
-                }).catch( error => { 
-                  console.log(error);
-                });
-
-    }else if(request.session.ubicacion === 'compra')
-    {
-        Proceso_CV.fetch_casasC_idC(request.session.user)
-            .then(([rows, fieldData]) => {
-                
-                response.render(path.join('..','views','op_venta','casasV.ejs'), {
-                    casas_V: rows[0],
-                    info: request.session.info ? request.session.info : '',
-                    isLoggedIn: request.session.isLoggedIN ? request.session.isLoggedIN : false,
-                    user: request.session.user ? request.session.user : '',
-                    ubicacion: request.session.ubicacion ? request.session.ubicacion : '',
-                    nombre: request.session.nombre ? request.session.nombre : '',
-                    registro: request.session. registro ? request.session. registro : '',
-
-
-                }); 
-            }).catch( error => { 
-              console.log(error)
-            });
-
-    }
-  
-};
 
 exports.get_vistasdocs = (request, response, next) => {
  //   request.session.ubicacion = request.params.operacion;
     request.session.docs = request.params.nombre_doc;
+    request.session.ubicacion = request.params.operacion;
+    request.session.idexp = request.params.tipo_exp;
+  //  console.log(request.params.nombre_doc,request.params.operacion,request.params.tipo_exp);
      if (request.session.docs == '1')
      {
 
@@ -377,6 +323,8 @@ exports.get_vistasdocs = (request, response, next) => {
         ubicacion: request.session.ubicacion ? request.session.ubicacion : '',
         nombre: request.session.nombre ? request.session.nombre : '',
         registro: request.session. registro ? request.session. registro : '',
+        idocs: request.session.docs ? request.session.docs : '',
+        idexp: request.session.idexp ? request.session.idexp : '',
         
 
     });
@@ -384,3 +332,12 @@ exports.get_vistasdocs = (request, response, next) => {
   
 };
 
+exports.post_docs = (request, response, next) => {
+//    request.session.docs = request.params.nombre_doc;
+  //  console.log(request.session.user,request.params.nombre_doc,request.file.filename,request.session.id_tip_expC,request.session.id_tip_DdocC);
+  console.log('Hola'+request.params.nombre_doc,request.params.operacion,request.params.tipo_exp);
+    const expediente = new ExpedienteRenta(request.session.user, request.params.nombre_doc, request.params.tipo_exp , 'En Revision', request.file.filename); 
+    expediente.save(); 
+
+     response.redirect('/user/perfil');
+};
